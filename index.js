4d442ef69bdda64cdd5c7d6e9828ba400027cfe0
rylor <pzh9me@virginia.edu>
2024-08-17 15:45:42 -0400

    Challenge 5 in the works

--- index.js
+++ index.js
@@ -124,9 +124,14 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
+    let listUnfundedGames = GAMES_JSON.filter( (game) => {
+        return game.pledged < game.goal;
+    });
+    //console.log(unfundedGames.length);
 
 
     // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(listUnfundedGames);
 
 }
 
@@ -135,9 +140,13 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
+    let listFundedGames = GAMES_JSON.filter( (game) => {
+        return game.pledged > game.goal;
+    });
 
 
     // use the function we previously created to add unfunded games to the DOM
+    addGamesToPage(listFundedGames);
 
 }
 
@@ -146,6 +155,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
